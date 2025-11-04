aba66b92cd6f1a56351ef6877e6b9d8d7aad22aa
Rounak Saeedi <ronaksaeedi65@gmail.com>
2025-11-04 10:20:28 +0100

    ronak bin

---
+++ app.js
@@ -0,0 +1,15 @@
+const ergebnis_container = document.querySelector(".ergebnis_container");
+const Ergebnis = document.querySelector("#Ergebnis");
+const text = document.querySelector(".text");
+const btn =document.querySelector(".btn");
+
+ergebnis_container.addEventListener("click",() =>{
+  const result= result();
+  console.log(result())
+});
+function result(){
+  document.getElementById("quiz-arry");
+  console.log()
+
+}
+
