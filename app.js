f157b082566b84fbc7296a8f07c77b0924424cc1
nai12-cr <flower_18@hotmail.de>
2025-11-04 12:59:34 +0100

    fragen

---
+++ app.js
@@ -0,0 +1,94 @@
+
+// Create an array of question objects.
+// Each question should contain:
+// - question text
+// - array of answer options with text and a "correct" boolean
+// Example structure:
+
+const questions = [
+  {
+    question: " Was ist JavaScript?",
+    answers: [
+      { text: "Styling web pages", correct: false },
+      { text: "Eine Programmiersprache für Webseiten.", correct: true },
+      { text: "Database management", correct: false }
+    ]
+  },
+   {
+    question: " Wann wurde JavaScript erfunden?",
+    answers: [
+      { text: "Styling web pages", correct: false },
+      { text: "JavaScript wurde im Jahr 1995 erfunden.", correct: true },
+      { text: "Database management", correct: false }
+    ]
+  },
+
+   {
+    question: " Wer hat JavaScript erfunden?",
+    answers: [
+      { text: "Styling web pages", correct: false },
+      { text: " Brendan Eich", correct: true },
+      { text: "Database management", correct: false }
+    ]
+  },
+  {
+    question: "Für welche Firma arbeitete er damals?",
+    answers: [
+      { text: "Styling web pages", correct: false },
+      { text: " Netscape", correct: true },
+      { text: "Database management", correct: false }
+    ]
+  },
+  {
+    question: " Wie lange hat die Entwicklung der ersten Version von JavaScript gedauert?",
+    answers: [
+      { text: "Styling web pages", correct: false },
+      { text: "Erstaunlicherweise nur 10 Tage – Brendan Eich schrieb die erste Version in nur zehn Tagen im Mai 1995.", correct: true },
+      { text: "Database management", correct: false }
+    ]
+  },
+ {
+    question: " Wie hieß JavaScript zuerst?",
+    answers: [
+      { text: "Styling web pages", correct: false },
+      { text: "Mocha, später LiveScript, dann JavaScript", correct: true },
+      { text: "Database management", correct: false }
+    ]
+  },
+  {
+    question: "  Warum wurde JavaScript erfunden?",
+    answers: [
+      { text: "Styling web pages", correct: false },
+      { text: "Um Webseiten interaktiver zu machen", correct: true },
+      { text: "Database management", correct: false }
+    ]
+  },
+  {
+    question: "  Ist JavaScript das gleiche wie Java?",
+    answers: [
+      { text: "Styling web pages", correct: false },
+      { text: "Nein, das sind zwei verschiedene Sprachen.", correct: true },
+      { text: "Database management", correct: false }
+    ]
+  },
+ {
+    question: " Was ist ein String?",
+    answers: [
+      { text: "Styling web pages", correct: false },
+      { text: "Ein Text in Anführungszeichen", correct: true },
+      { text: "Database management", correct: false }
+    ]
+  },
+  {
+    question: " Was ist ein Boolean?",
+    answers: [
+      { text: "Styling web pages", correct: false },
+      { text: "Ein Wert, der nur wahr (true) oder falsch (false) sein kann", correct: true },
+      { text: "Database management", correct: false }
+    ]
+  },
+ 
+
+
+];
+
